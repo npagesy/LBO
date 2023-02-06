bba6b404859127da00f0ecdaf202264d0819e5d5
Noeline360 <noeline.pagesy@360medics.com>
2023-02-06 18:16:27 -0400

    Initial Commit

---
+++ LBO/LBOApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBOApp.swift
+//  LBO
+//
+//  Created by Noeline PAGESY on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LBOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
